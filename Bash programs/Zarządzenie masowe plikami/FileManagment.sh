0a7806c313fff6798cf57f8d4ff1236d1070a23b
MBernackiProgrammer <101584672+MBernackiProgrammer@users.noreply.github.com>
2022-10-24 14:29:18 +0200

    Ready

--- Bash programs/Zarządzenie masowe plikami/FileManagment.sh
+++ Bash programs/Zarządzenie masowe plikami/FileManagment.sh
@@ -2,6 +2,7 @@
 
 taskNb=0
 pathWork=""
+#Print menu 
 echo Drogi użytkowniku, wygrałeś iphone 9,
 echo ale zanim to to musisz wybrać mi zadanie
 echo 1 - zmien nazwy plików w katalogu na wielkie 
@@ -9,36 +10,57 @@ echo 2 - zmien nazwy plików na małe litery
 echo 3 - skróć nazwy plików 
 echo 4 - wysczyść zawartość plików 
 
+#Get option 
 read taskNb
 
 echo Hej, proszę podaj jeszcze ścieżkę, na której mam pracować
 
-
+#Get path 
 read pathWork
 
 echo $pathWork
 
+#Create copy 
 rm -r /home/test2/PathChangerHelper
 cp -R "$pathWork" "/home/test2/PathChangerHelper"
 
+
+#If is anweser 1 - change to BIG letter
 if [ $taskNb -eq 1 ]
 then 
 	for entry in `ls "$pathWork"`; do
     		echo $entry
     		
+    		noteste=$pathWork/$entry
     		teste=$pathWork/${entry^^}
+    		echo $teste
     		
-    		if [[ -f  $teste ]]
+    		if [[ -f "$teste" ]]
 		then 
-			echo znaleziono taki sam plik, który miał by taką samą nazwę po zmianie pod ścieżką $pathWork/$entry
-    			echo wpisz, byle co, abym wiedział, że ty wiesz
-    			read 
+			if [[ "$noteste" = "$teste" ]]
+			then
+				echo Plik "$teste" nie musi zostać zmieniony
+			else
+				echo znaleziono taki sam plik, który miał by taką samą nazwę po zmianie pod ścieżką $pathWork/$entry
+    				echo wpisz, byle co, abym wiedział, że ty wiesz
+    				read 
+			fi 
+			
 		else 
+			echo ten $teste plik spoko, nic
 			mv $pathWork/$entry $pathWork/${entry^^}
+		fi 
+		
+		if [[ -f "$teste" ]]
+		then 
+			echo mamy to 
+		else 
+			echo nie mamy tego FFF
 		fi 
 	done	
 fi
 
+#If is anweser 2 - change to small letter
 if [ $taskNb -eq 2 ]
 then 
 	for entry in `ls "$pathWork"`; do
@@ -92,8 +114,8 @@ echo Czy zachować zmiany T/N
 read Odp 
 if [ Odp = "T" ]
 then
-	#Nic nie rób 
-	echo nic
+	#Nic nie rób
+	rm -r "/home/test2/PathChangerHelper"
 fi 
 
 if [ Odp = "N" ]
